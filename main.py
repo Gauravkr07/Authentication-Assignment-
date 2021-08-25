056101164175f24fd315cc84ba3c9735c7b843ad
Gaurav <Gauravap70@gmail.com>
2021-08-25 18:46:10 +0530

    sec commit

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-
+import secrets
 from pydantic import BaseModel
 from pymongo import MongoClient
-from fastapi import FastAPI,Depends,HTTPException
+from fastapi import FastAPI,Depends,HTTPException,status
 from passlib.context import CryptContext
-from fastapi.security import  OAuth2PasswordBearer,OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm, HTTPBasicCredentials, HTTPBasic
 #from mongoengine import Document,StringField
 from enum import unique
 from pymongo.errors import DuplicateKeyError
 
+
+security = HTTPBasic()
 app=FastAPI()
 client=MongoClient("mongodb://localhost:27017/")
 db=client["Userinfo"]
@@ -26,13 +28,19 @@ def welcome(token:str=Depends(oauth_scheme)):
     return{"token":token}
 #==================================================================================================================================
 @app.post("/sign_up",tags=["Authentication"])
-async def sign_up(new_user:New_user):
-    try:
-        mycoll.insert({"username":new_user.username ,"password":hasshing(new_user.password)})
-        return {"Successfully Registered"}
-    except DuplicateKeyError:
-        print("Username is already present.Try another")
-        return {"DuplicateKeyError"}
+
+async def sign_up(new_user:New_user,credentials: HTTPBasicCredentials = Depends(security)):
+    correct_username = secrets.compare_digest(credentials.username, "hel")
+    correct_password = secrets.compare_digest(credentials.password, "say")
+    if not (correct_username and correct_password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Incorrect email or password",headers={"WWW-Authenticate": "Basic"},)
+    else:
+        try:
+            mycoll.insert({"username":new_user.username ,"password":hasshing(new_user.password)})
+            return {"Successfully Registered"}
+        except DuplicateKeyError:
+            print("Username is already present.Try another")
+            return {"DuplicateKeyError"}
 
 def hasshing(password):
     return pwd_context.hash(password)
@@ -49,11 +57,11 @@ async def sign_in(form_data:OAuth2PasswordRequestForm=Depends()):
 async def authenticate(username,password):
     import ipdb;
     ipdb.set_trace()
-    user=mycoll.find_one({"username":username})
-
+    user=  mycoll.find_one({"username":username})
     if user:
-         veri=pwd_context.verify(password,**(mycoll.find_one(password)))
-         return veri
-    # user=json.loads(User.objects.get(username=username).to_json())
+        #mycoll.aggregate()
+        veri=pwd_context.verify(password,**(mycoll.find_one(username,password)))
+        print(veri)
+        return  veri
     else:
         return False
