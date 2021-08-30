065b72042d43c6710c214ed15756e96229271cf0
Gaurav <Gauravap70@gmail.com>
2021-08-30 22:47:50 +0530

    env var 5

--- config.py
+++
@@ -1,9 +0,0 @@
-
-database="Userinfo"
-database_server="mongodb://localhost:27017/"
-
-#=============================================================
-sign_up_username="hel"
-sign_up_password="say"
-
-#=======================================
