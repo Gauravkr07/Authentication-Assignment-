97c3f70a278f9c8cc5bd6dafcc757515fb25a682
Gaurav <Gauravap70@gmail.com>
2021-08-30 21:46:12 +0530

    gitintit commit 4

---
+++ config.py
@@ -0,0 +1,9 @@
+
+database="Userinfo"
+database_server="mongodb://localhost:27017/"
+
+#=============================================================
+sign_up_username="hel"
+sign_up_password="say"
+
+#=======================================
